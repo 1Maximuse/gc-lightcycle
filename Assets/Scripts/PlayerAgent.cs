cf789bcb464bfd5c4eb8ab2fcfd790de5fb2d780
Emmanuel Maximus Yohanes <geofanny.emmanuel@gmail.com>
2021-06-05 12:52:02 +0700

    initial project + movement

---
+++ Assets/Scripts/PlayerAgent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using UnityEngine;
+
+public class PlayerAgent : Agent
+{
+    [SerializeField]
+    private float accelerateSpeed;
+    [SerializeField]
+    private float brakeSpeed;
+    [SerializeField]
+    private float turnSpeed;
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float tiltAmount;
+    [SerializeField]
+    private Transform model;
+
+    private Rigidbody rigidBody;
+
+    public override void Initialize()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // private float agentRunSpeed = 2f;
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        ActionSegment<int> act = actions.DiscreteActions;
+        Vector3 force = Vector3.zero;
+        Vector3 rotateDir = Vector3.zero;
+
+        Debug.Log(act[0] + ", " + act[1]);
+
+        float forwardSpeed = Vector3.Dot(rigidBody.velocity, transform.forward);
+        if (act[0] == 1 && forwardSpeed < maxSpeed) // Accelerate
+            rigidBody.AddForce(transform.forward * accelerateSpeed, ForceMode.Force);
+        else if (act[0] == 2 && forwardSpeed > -maxSpeed / 2) // Brake
+            rigidBody.AddForce(-transform.forward * brakeSpeed, ForceMode.Force);
+
+        if (act[1] == 1) // Left
+            rigidBody.AddTorque(transform.up * -turnSpeed);
+        else if (act[1] == 2) // Right
+            rigidBody.AddTorque(transform.up * turnSpeed);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = Input.GetKey(KeyCode.W) ? 1 : (Input.GetKey(KeyCode.S) ? 2 : 0);
+        discreteActions[1] = Input.GetKey(KeyCode.A) ? 1 : (Input.GetKey(KeyCode.D) ? 2 : 0);
+    }
+
+    void Update()
+    {
+        float forwardSpeed = Vector3.Dot(rigidBody.velocity, transform.forward);
+        float sidewaysSpeed = Vector3.Dot(rigidBody.velocity, transform.right);
+        float rotateSpeed = rigidBody.angularVelocity.y;
+        rigidBody.AddForce(-transform.right * sidewaysSpeed, ForceMode.Force);
+
+        model.localRotation = Quaternion.Euler(0, 0, -rotateSpeed * forwardSpeed * tiltAmount);
+    }
+}
---
+++ Assets/Scripts/TrailGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailGenerator : MonoBehaviour
+{
+    private Vector3 prevPos;
+
+    void Start()
+    {
+        prevPos = transform.position;
+        InvokeRepeating("CalculateDistance", 0, 0.1f);
+    }
+
+    void CalculateDistance()
+    {
+        Vector3 currentPos = transform.position;
+
+        Debug.Log((currentPos - prevPos).sqrMagnitude);
+
+        prevPos = currentPos;
+    }
+}
